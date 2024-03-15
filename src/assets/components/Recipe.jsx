ca2e9c3c32f41d5413df44234f74e805cd019147
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-16 00:08:41 +0600

    Created a separate sidebar!

--- src/assets/components/Recipe.jsx
+++ src/assets/components/Recipe.jsx
@@ -1,3 +1,5 @@
+import Sidebar from "./Sidebar";
+
 const Recipe = () => {
     return (
         <div className="mt-24">
@@ -114,78 +116,7 @@ const Recipe = () => {
 
                         </div>
                     </div>
-                    <div className="space-y-8 rounded-2xl border border-[#28282833] ">
-                        <div>
-                            <div className="space-y-4 w-full mt-8 px-20">
-                                <h1 className="text-center font-semibold lexend text-[#282828] text-2xl">Want to cook: 01</h1>
-                                <hr className="w-full" />
-                            </div>
-                            <div>
-                                <div className="overflow-x-auto fira">
-                                    <table className="table">
-                                        <thead>
-                                            <tr className="text-[#878787] fira text-base font-medium">
-                                                <th></th>
-                                                <th>Name</th>
-                                                <th>Time</th>
-                                                <th>Calories</th>
-                                                <th></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody className="text-[#282828b3] fira text-base">
-                                            <tr className="bg-[#28282808]">
-                                                <th>1</th>
-                                                <td>Chicken Caesar Salad</td>
-                                                <td>20 minutes</td>
-                                                <td>400 calories</td>
-                                                <td><button className="btn rounded-full bg-[#0BE58A] px-4 text-[#150B2B] lexend">Preparing</button></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="space-y-4 w-full">
-                                <h1 className="text-center font-semibold lexend text-[#282828] text-2xl">Want to cook: 01</h1>
-                                <hr className="w-full px-12" />
-                            </div>
-                            <div>
-                                <div className="overflow-x-auto fira">
-                                    <table className="table">
-                                        <thead>
-                                            <tr className="text-[#878787] fira text-base font-medium">
-                                                <th></th>
-                                                <th>Name</th>
-                                                <th>Time</th>
-                                                <th>Calories</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody className="text-[#282828b3] fira text-base">
-                                            <tr className="bg-[#28282808]">
-                                                <th>1</th>
-                                                <td>Chicken Caesar Salad</td>
-                                                <td>20 minutes</td>
-                                                <td>400 calories</td>
-                                            </tr>
-                                            <tr className="bg-[#28282808]">
-                                                <th>1</th>
-                                                <td>Chicken Caesar Salad</td>
-                                                <td>20 minutes</td>
-                                                <td>400 calories</td>
-                                            </tr>
-                                            <tr className="lexend font-base font-medium text-[#282828cc]">
-                                                <th></th>
-                                                <td></td>
-                                                <td>Total Time = 20 minutes</td>
-                                                <td>Total Calories = 400 calories</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Sidebar></Sidebar>
                 </div>
             </div>
         </div>
