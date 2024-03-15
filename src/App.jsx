55ad1e3a7aec0cf091f8e47839cb0e8155e03d10
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-16 01:16:47 +0600

    Api Fetching recipe data from json!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import HeroSection from './assets/components/Hero'
 import Navbar from './assets/components/Navbar'
-import Recipe from './assets/components/Recipe'
+import Recipes from './assets/components/Recipes'
 function App() {
   return (
     <>
       <div className='container mx-auto'>
         <Navbar></Navbar>
         <HeroSection></HeroSection>
-        <Recipe></Recipe>
+        <Recipes></Recipes>
       </div>
     </>
   )
---
+++ src/assets/components/Recipes.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Sidebar from "./Sidebar";
+import { useEffect } from "react";
+import Recipe from "./Recipe";
+
+const Recipes = () => {
+    const [recipes, setRecipe] = useState([]);
+    useEffect(() => {
+        fetch('recipe_data.json')
+            .then(res => res.json())
+            .then(data => setRecipe(data))
+    }, [])
+    return (
+        <div className="mt-24">
+            <div className="lexend space-y-6">
+                <h1 className="text-center text-4xl font-semibold text-[#150B2B]">Our Recipes</h1>
+                <p className="text-center px-56 text-[#150b2b99] leading-7">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+                <div className="flex gap-6 mt-12">
+                    <div>
+                        <div className="grid grid-cols-2 gap-6">
+                            {
+                                recipes.map(recipe => <Recipe key={recipe.recipe_id} recipe={recipe}></Recipe>)
+                            }
+                        </div>
+                    </div>
+                    <Sidebar></Sidebar>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Recipes;
